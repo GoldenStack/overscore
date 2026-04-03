bfedc998a99cf7bf9af07255f7cdc25369c4113e
GoldenStack <git@goldenstack.net>
2026-04-03 15:11:03 -0500

    Implement the second translation phase

--- src/c/translation.zig
+++ src/c/translation.zig
@@ -1,6 +1,8 @@
 //! Implements the 8 translation phases described in ANSI C89 5.1.1.2.
 //! These are implemented lazily, as to allow all phases to occur in one pass.
 //! Each phase depends on the previous phase, querying from it when necessary.
+//! They could be merged for performance, but that's something to be dealt with
+//! in the future.
 
 const std = @import("std");
 const lex = @import("../lex.zig");
@@ -90,5 +92,51 @@ pub const Phase1 = struct {
         }
 
         return char;
+    }
+};
+
+/// Implements phase 1, where newline behaviour is checked & enforced.
+/// Specifically, escaped newlines are mapped out and the file must end with a
+/// newline.
+pub const Phase2 = struct {
+    phase1: Phase1,
+    next_char: ?u8,
+
+    /// Initializes a new phase 2 parser.
+    pub fn init(src: [:0]const u8) @This() {
+        return .{
+            .phase1 = Phase1.init(src),
+            .next_char = null,
+        };
+    }
+
+    /// Returns the location of this phase.
+    pub fn location(self: *const @This()) lex.Location {
+        return self.phase1.location();
+    }
+
+    /// Peeks at the next character in the source string.
+    pub fn peek(self: *const @This()) u8 {
+        if (self.next_char) |char| return char;
+
+        const start = self.location();
+        const char = self.next();
+
+        self.next_char = char;
+        self.phase1.loc = start;
+
+        return char;
+    }
+
+    /// Pops the next character in the source string.
+    /// No-op if already at the end of the string.
+    pub fn next(self: *@This()) u8 {
+        const char = self.phase1.next();
+
+        // Test for removed newline
+        if (char == '\\' and self.phase1.peek() == '\n') {
+            _ = self.phase1.next();
+            return self.next();
+        } else return char;
     }
 };
